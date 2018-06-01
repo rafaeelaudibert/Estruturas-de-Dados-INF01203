65946abd649a72369d8cb9a3fbdde30dbeb2d545
RafaAudibert <rafaeelaudibert@gmail.com>
2018-05-31 22:24:22 -0300

    Starting to make BST functions

---
+++ arvore.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "arvore.h"
+
+/// Funçao responsável por criar a árvore
+/// OUTPUT:
+///         Consulta*            ->  Árvore zerada / Ponteiro NULL
+Consulta* criaArvore(){
+    return NULL;
+}
---
+++ arvore.h
@@ -0,0 +1,7 @@
+#include "struct.h"
+
+#ifndef ARVORE_H_
+#define ARVORE_H_
+
+Consulta* criaArvore();
+#endif // ARVORE_H_
